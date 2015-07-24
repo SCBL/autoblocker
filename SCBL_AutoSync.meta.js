6cd4a351b4951daf4afe3d7d16ce893717bbf6e3
PotcFdk <PotcFdk@gmx.net>
2015-07-25 01:19:37 +0200

    Add hack to make it compatible with Greasemonkey on Firefox.

--- SCBL_AutoSync.meta.js
+++ SCBL_AutoSync.meta.js
@@ -1,14 +1,12 @@
 // ==UserScript==
 // @author      PotcFdk
 // @name        SCBL Autoblocker
-// @version     0.0.1
+// @version     0.0.2
 // @namespace   https://github.com/SCBL/autoblocker
 // @description Auto-ignores all players in the SCBL blocklist.
 // @match       http://steamcommunity.com/*
 // @match       https://steamcommunity.com/*
 // @grant       GM_xmlhttpRequest
-// @grant       GM_getValue
-// @grant       GM_setValue
 // @downloadURL https://raw.githubusercontent.com/SCBL/autoblocker/master/SCBL_AutoSync.user.js
 // @updateURL   https://raw.githubusercontent.com/SCBL/autoblocker/master/SCBL_AutoSync.meta.js
 // ==/UserScript==
