cb3ca7cc2e7d288ca393b5215b9bf1a5a50f8f2a
PotcFdk <PotcFdk@gmx.net>
2015-09-08 03:22:36 +0200

    Bump version.

--- SCBL_AutoSync.user.js
+++ SCBL_AutoSync.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author      PotcFdk
 // @name        SCBL Autoblocker
-// @version     0.0.2
+// @version     0.0.3
 // @namespace   https://github.com/SCBL/autoblocker
 // @description Auto-ignores all players in the SCBL blocklist.
 // @match       http://steamcommunity.com/*
